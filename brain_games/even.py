c503280e0f2c6b82b7a1474d2cad9ed8b8418d71
Julia Mezenova <julia21moon86@mail.ru>
2023-03-17 21:43:03 +0500

    update brain_games/even.py

--- brain_games/even.py
+++ brain_games/even.py
@@ -1,14 +1,12 @@
-def start_game():
-    start_game = 'Answer "yes" if the number is even, otherwise answer "no".'
-    return print(start_game)
-
-
 import prompt
 import random
-from brain_games.cli import name
+from brain_games.cli import welcome_user
 
 
 def answer_from_user():
+    name = welcome_user()
+    start_game = 'Answer "yes" if the number is even, otherwise answer "no".'
+    print(start_game)
     question = random.randint(1, 100)
     def is_right_answer(question):
         right_answer = ''  
@@ -22,7 +20,13 @@ def answer_from_user():
     n = 1
     while answer == is_right_answer(question) and n < 3:
         print('Correct!')
-        n+=1                                                                                                                    question = random.randint(1, 100)                                                                                       print("Question: " + str(question))
-        answer = prompt.string("Your answer: ")                                                                                 is_right_answer(question)                                                                                           if answer != is_right_answer(question):
+        n+=1
+        question = random.randint(1, 100)
+        print("Question: " + str(question))
+        answer = prompt.string("Your answer: ")
+        is_right_answer(question)
+    if answer != is_right_answer(question):
         print("'" + answer + "'" + " is wrong answer ;(. Correct answer was '" + is_right_answer(question) + "'.\nLet\'s try again, " + name + "!")
-        print("")                                                                                                           else:                                                                                                                       print('Congratulations, ' + name +'!')
+        print("")
+    else:
+        print('Congratulations, ' + name +'!')
