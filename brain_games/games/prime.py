90976498414f2f4fc9125a6b27e8b0a4aad150d2
Julia Mezenova <julia21moon86@mail.ru>
2023-03-26 18:13:23 +0500

    add Brain-prime

---
+++ brain_games/games/prime.py
@@ -0,0 +1,17 @@
+import random
+import sympy
+
+
+def start_game():
+    start_game = 'Answer "yes" if given number is prime. Otherwise answer "no".'
+    print(start_game)
+
+
+def game_logic():
+    question = random.randint(1, 200)
+    if sympy.isprime(question) == True:
+        right_answer = 'yes'
+    else:
+        right_answer = 'no'
+    return (question, right_answer)
+
