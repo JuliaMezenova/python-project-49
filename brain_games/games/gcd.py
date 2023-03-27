fbe485ad3b369b99304602323c15057a7d958e2a
Julia Mezenova <julia21moon86@mail.ru>
2023-03-27 20:19:58 +0500

    delete error in gcd.py

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -10,6 +10,6 @@ def start_game():
 def game_logic():
     num1 = random.randint(1, 100)
     num2 = random.randint(1, 100)
-    question = f'{num1}  {num2}'
+    question = f'{num1} {num2}'
     right_answer = gcd(num1, num2)
     return question, right_answer
