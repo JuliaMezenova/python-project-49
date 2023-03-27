efba732e39ed7be7ee6585e87ffe3adde3a30edb
Julia Mezenova <julia21moon86@mail.ru>
2023-03-27 20:04:52 +0500

    delete error in calc.py

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -10,6 +10,6 @@ def game_logic():
     operand1 = random.randint(1, 100)
     operator = random.choice(['+', '-', '*'])
     operand2 = random.randint(1, 100)
-    question = str(operand1) + operator + str(operand2)
+    question = str(operand1) + ' ' + operator + ' ' + str(operand2)
     right_answer = eval(question)
     return (question, right_answer)
