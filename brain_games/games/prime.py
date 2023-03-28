fabd19b965de8865e73b829e5ac09d332c2dcd7b
Julia Mezenova <julia21moon86@mail.ru>
2023-03-28 15:02:03 +0500

    delete lint error from brain_games/games/prime.py

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -9,7 +9,7 @@ def start_game():
 def game_logic():
     question = random.randint(2, 200)
     counter = 0
-    for divisor in range(2, question // 2+1):
+    for divisor in range(2, question // 2 + 1):
         if (question % divisor == 0):
             counter += 1
     if counter <= 0:
